3dcc6e83d923bebc1d061a8c3e400880e4525fa6
K0rgana <anagrom1999@gmail.com>
2020-09-13 15:06:23 -0300

    adiciona volume total de negociações

--- client/app/domain/negociacao/Negociacoes.js
+++ client/app/domain/negociacao/Negociacoes.js
@@ -2,9 +2,17 @@ class Negociacoes{
     constructor(){
         this._negociacoes = [];
     }
+
+    get volumeTotal(){
+        return this._negociacoes
+                .reduce((total, negociacao) => 
+                    total + negociacao.volume, 0);
+    }
+
     adiciona(negociacao){
         this._negociacoes.push(negociacao);
     }
+
     paraArray(){
         // retorna uma nova referência criada com os itens de this._negociacoes
         return [].concat(this._negociacoes);
